ebea31c881ced91b179b0581e66e5e688707c726
ABOUSALHAM AMINE p1908955 <amine.abousalham@etu.univ-lyon1.fr>
2020-09-18 00:28:46 +0200

    charte graphique 3

--- app/src/main/java/com/example/ptut_application_mobile_sportetu/MainActivity.kt
+++ app/src/main/java/com/example/ptut_application_mobile_sportetu/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ptut_application_mobile_sportetu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,10 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val ProgressionFragment = ProgressionFragment()
+        val SuccesFragment = SuccesFragment()
+        val EntrainementFragment = EntrainementFragment()
 
+        makeCurrentFragment(ProgressionFragment)
 
-
-
+        bottom_navigation.setOnNavigationItemSelectedListener{
+            when (it.itemId){
+                R.id.nav_progression->makeCurrentFragment(ProgressionFragment)
+                R.id.nav_succes->makeCurrentFragment(SuccesFragment)
+                R.id.nav_entrainement->makeCurrentFragment(EntrainementFragment)
+            }
+            true
+        }
     }
 
+    private fun makeCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.fragment_container,fragment)
+            commit()
+        }
 }
