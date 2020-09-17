f8d50b51261e872c71c9d681c2fdcb16095c63b7
ABOUSALHAM AMINE p1908955 <amine.abousalham@etu.univ-lyon1.fr>
2020-09-17 23:26:07 +0200

    charte graphique 2

--- app/src/main/java/com/example/ptut_application_mobile_sportetu/MainActivity.kt
+++ app/src/main/java/com/example/ptut_application_mobile_sportetu/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.ptut_application_mobile_sportetu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
     }
+
 }
